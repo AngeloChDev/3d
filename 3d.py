63daeecc521e90316b77bfb43b45102d4d439824
AngeloChDev <angeloch.dev@gmail.com>
2023-02-09 17:45:22 +0100

    init

---
+++ 3d.py
@@ -0,0 +1,17 @@
+from ursina import *
+
+
+app = Ursina()                                  #### inizialize app 3d
+
+cube = Entity(model='cube', color=color.green, texture='withe_cube', scale=2 )    ### create a identity object
+
+def update():
+    cube.rotation_x = cube.rotation_x + 0.25
+    cube.rotation_y = cube.rotation_y + 0.25
+    cube.x += held_keys['a'] * 0.2
+    cube.x -= held_keys['s'] * 0.2
+    cube.y += held_keys['q'] * 0.2
+    cube.y -= held_keys['w'] * 0.2
+    
+app.run()
+    
