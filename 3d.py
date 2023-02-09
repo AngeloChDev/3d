8e27d39f04c4f853869179fd14f03e4fcec0eed0
AngeloChDev <angeloch.dev@gmail.com>
2023-02-09 17:46:54 +0100

    init

--- 3d.py
+++ 3d.py
@@ -8,10 +8,10 @@ cube = Entity(model='cube', color=color.green, texture='withe_cube', scale=2 )  
 def update():
     cube.rotation_x = cube.rotation_x + 0.25
     cube.rotation_y = cube.rotation_y + 0.25
-    cube.x += held_keys['a'] * 0.2
-    cube.x -= held_keys['s'] * 0.2
+    cube.x += held_keys['s'] * 0.2
+    cube.x -= held_keys['a'] * 0.2
     cube.y += held_keys['q'] * 0.2
-    cube.y -= held_keys['w'] * 0.2
+    cube.y -= held_keys['y'] * 0.2
     
 app.run()
     
